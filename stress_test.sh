3b793e62addf98ac502a1ac677191e213b843e9e
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2023-10-03 23:53:29 +0530

    Update stress_test.sh

--- stress_test.sh
+++ stress_test.sh
@@ -1,39 +1,32 @@
-#!/bin/bash
+# Use an alpine base image for minimal size
+FROM alpine:latest
 
-cleanup() {
-    echo "Received abort signal. Cleaning up..."
-    kubectl exec -n $NAMESPACE $POD_NAME -- rm -f /tmp/stressfile
-    exit 1
-}
+# Install kubectl
+RUN apk add --no-cache curl && \
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
+    chmod +x kubectl && \
+    mv kubectl /usr/local/bin/ && \
+    apk del curl
 
-# Handle abort signals
-trap 'cleanup' SIGINT SIGTERM
+# Install stress-ng from source
+RUN apk add --no-cache build-base linux-headers git zlib-dev && \
+    git clone https://github.com/ColinIanKing/stress-ng.git && \
+    cd stress-ng && \
+    make && \
+    make install && \
+    cd .. && \
+    rm -rf stress-ng && \
+    apk del build-base linux-headers git zlib-dev
 
-# Check if we have the required arguments
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <pod_name> <namespace> <memory_in_mb> <time_in_seconds>"
-    exit 1
-fi
+# Copy the script into the container
+COPY stress_test.sh .
+RUN chmod +x stress_test.sh
 
-POD_NAME=$1
-NAMESPACE=$2
-MEMORY_MB=$3
-DURATION=$4
+# Set environment variables for the arguments; these can be overridden at runtime
+ENV POD_NAME=podname
+ENV NAMESPACE=default
+ENV MEMORY_MB=100
+ENV DURATION=60
 
-# Exec into the pod and run the stress commands
-
-# Stress Memory using dd
-echo "Stressing memory for ${DURATION} seconds..."
-kubectl exec -n $NAMESPACE $POD_NAME -- bash -c "dd if=/dev/zero of=/tmp/stressfile bs=1M count=$MEMORY_MB & sleep $DURATION && rm -f /tmp/stressfile" &
-
-# Store the background process PID
-BACKGROUND_PID=$!
-
-# If you wish to add CPU stress, consider using a tool like 'stress' or 'stress-ng'
-# Example:
-# kubectl exec -n $NAMESPACE $POD_NAME -- bash -c "stress --cpu 1 --timeout ${DURATION}s"
-
-# Wait for the commands to complete
-wait $BACKGROUND_PID
-
-echo "Stress test completed."
+# The script will be our entry point
+ENTRYPOINT ["./stress_test.sh", "$POD_NAME", "$NAMESPACE", "$MEMORY_MB", "$DURATION"]
